afbcfac4c611cc0d45581f3da49275655981a6dc
Milton Júnior <jrsinhorini@gmail.com>
2026-04-22 01:23:59 -0300

    Feat pag Esqueci senha

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Estabelecimento from "./pages/Estabelecimento/index.jsx";
 import ConfiguracaoEstabelecimento from "./pages/Estabelecimento/Configuracao/index.jsx";
 import PerfilEstabelecimento from "./pages/Estabelecimento/Perfil.jsx";
 import Login from "./pages/Login/index.jsx";
+import EsqueciSenha from "./pages/Login/EsqueciSenha/index.jsx";
 import Cadastro from "./pages/Cadastro/index.jsx";
 import Profissional from "./pages/Profissional/index.jsx";
 import ConfiguracaoProfissional from "./pages/Profissional/Configuracao/index.jsx";
@@ -32,9 +33,7 @@ function App() {
         <Route path="/profissional/configuracoes" element={<ConfiguracaoProfissional />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-        <Route path="/redefinir-senha" element={<RedefinirSenha />} />
-        <Route path="/confirmar-email" element={<ConfirmarEmail />} /> */}
+        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
       </Routes>
     </AppLayout>
   );
