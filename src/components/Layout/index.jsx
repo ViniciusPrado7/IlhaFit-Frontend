87d584ef7c9736f407e33469b609bbe6b234cd82
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2026-05-20 22:34:40 -0300

    feat: adiciona footer institucional com botao de voltar ao topo

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,14 +1,17 @@
 import { Box } from "@mui/material";
+import Footer from "../Footer";
 import LayoutContainer from "./LayoutContainer";
 
 const AppLayout = ({ children }) => {
   return (
     <Box sx={{ bgcolor: "background.default", flex: 1 }}>
+      <Box id="app-top" sx={{ position: "relative", top: -80 }} />
       <LayoutContainer>
         <Box sx={{ pt: 14 }}>
           {children}
         </Box>
       </LayoutContainer>
+      <Footer />
     </Box>
   );
 };
